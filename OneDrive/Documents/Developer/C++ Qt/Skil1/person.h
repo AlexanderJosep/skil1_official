73570db6f09826d93274dc1bc4cde9c821941b0b
AlexanderJosep <12ajb@ma.is>
2016-11-28 10:27:00 +0000

    Test

---
+++ OneDrive/Documents/Developer/C++ Qt/Skil1/person.h
@@ -0,0 +1,18 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+
+class Person
+{
+public:
+    Person();
+private:
+    string name;
+    short sex; //0 male, 1 female
+    short birthYear;
+    short deathYear;
+};
+
+// nafn, kyn, fæðingarár og dánarár
+
+#endif // PERSON_H
